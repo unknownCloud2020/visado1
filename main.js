ce515d1478274c696cb47a0ec39bbd3984262bbb
Pablo Troche <trochepablodaniel@gmail.com>
2020-09-17 10:08:16 -0300

    refact: main legibilidad

--- main.js
+++ main.js
@@ -51,10 +51,10 @@ function saveUNQfy(unqfy, filename = 'data.json') {
 */
 
 function main() {
-  // console.log('arguments: ');
-  // process.argv.forEach(argument => console.log(argument));
   const commandIn = process.argv.slice(2).shift(); // obtengo el commando enviado por parametro, que es el tercer argumento
-  commands.get(commandIn.toUpperCase()).prototype.execute(process.argv.slice(3)); // obtengo el correspondiente commando para ejecutar su logica
+  const command = commands.get(commandIn.toUpperCase()).prototype; // obtengo la clase del comando por polimorfismo
+  const params = process.argv.slice(3); 
+  command.execute(params); // ejecuto comando y envio el resto de los argumentos
 }
 
 main();
