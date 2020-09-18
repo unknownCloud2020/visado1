d40c61bf57e932543dda2f588ad57b37b8fbe2c4
Pablo Troche <trochepablodaniel@gmail.com>
2020-09-18 11:54:02 -0300

    overwrite execute createPlaylist

--- src/commands/playlist/CreatePlaylistCommand.js
+++ src/commands/playlist/CreatePlaylistCommand.js
@@ -2,7 +2,8 @@ const Command = require('../Command');
 
 class CreatePlaylistCommand extends Command {
     execute(args) { 
-        this.unqfy.addArtist(this.paramsBuilder(args));
+        const playListArgs = this.paramsBuilder(args);
+        this.unqfy.createPlaylist(playListArgs.name, playListArgs.genresToInclude, playListArgs.maxDuration);
     }
 }
 
